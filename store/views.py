fceb36cea164a44094780745cee316ec61f1eb2c
MR3B152 <mr3b152@gmail.com>
2023-05-21 20:36:41 +0300

    #5 commit

--- store/views.py
+++ store/views.py
@@ -1,21 +1,46 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-
+from .models import Product, Slider, Category
 
 def index(request):
+    products = Product.objects.select_related('author').filter(featured=True)
+    slides = Slider.objects.order_by('order')
     return render(
-        request, 'index.html'
+        request, 'index.html',
+        {
+            'products': products,
+            'sliders': slides
+        }
     )
 
 
 def product(request, pid):
+    product = Product.objects.select_related()
     return render(
         request, 'product.html'
     )
 
 
-def category(request, cid):
+def category(request, cid=None):
+    cat = None
+    where = {}
+    if cid:
+        cat = Category.objects.get(pk=cid)
+        where['category_id'] = cid
+
+    products = Product.objects.filter(**where)
+    paginator = Paginator(products, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
     return render(
-        request, 'category.html'
+        request, 'category.html',
+        {
+            'page_obj': page_obj,
+            'category': cat
+            
+        }
     )
 
 
