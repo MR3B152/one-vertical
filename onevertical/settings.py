893e0086496fa87261df4d8a531d674bf773fa54
MR3B152 <mr3b152@gmail.com>
2024-06-04 21:48:25 +0300

    last commit

--- onevertical/settings.py
+++ onevertical/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0keyq!^ndlkqs@-73h3x3rm9&g-h(wqu2&m363w5@)i56*+pbf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
